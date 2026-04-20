8811dbc2e44f5805460bf51ac8ec03fecfd635aa
killioin007-source <killioin.007@gmail.com>
2026-04-20 15:35:04 -0400

    created other source files link

--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -1,17 +1,48 @@
 package com.pluralsight;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        System.out.println("""
+        What story do you wanna read?
+        1. Goldilocks
+        2. Hansel and Gretel
+        3. Mary had a little lamb""");
+        try {
+            Scanner scanners = new Scanner(System.in);
+            String userInput = scanners.nextLine();
+            String goldilocks ="src/main/resources/goldilocks.txt" ;
+            String hanselAndGretel ="src/main/resources/hansel_and_gretel.txt" ;
+            String maryAndLamb ="src/main/resources/mary_had_a_little_lamb.txt" ;
+            String filepath;
+            if (userInput.equals("goldilocks")) {
+                filepath = goldilocks;
+            } else if (userInput.equals("hansel and gretel")) {
+                filepath = hanselAndGretel;
+            } else if(userInput.equals("mary had a little lamb")) {
+                filepath = maryAndLamb;
+            }
+            scanners.close();
+            Scanner scanner = new Scanner(new File("src/main/resources/goldilocks.txt"));
+            String line = "";
+            int lineCount = 1;
+            while (scanner.hasNext()) {
+                line = scanner.nextLine();
+                System.out.println(lineCount + ".\t \t " + line);
+                lineCount++;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+            }
+
+        } catch (FileNotFoundException e) {
+            System.out.println("I couldn't find that file.");
         }
+
     }
+
+
 }
